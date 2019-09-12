53954b870db344439b69d48fced13cd4b92a9a77
Lucas Müllner <lucasmuellner@gmx.at>
2019-09-12 09:43:01 +0200

    commit HomeMatic_EXE project

---
+++ 004_HomeMatic_EXE/Client.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _004_HomeMatic_EXE
+{
+    class Client
+    {
+        static void Blub(string[] args)
+        {
+            try
+            {
+                // gets IPHostEntry from IP address of local computer
+                // IPHostEntry ... ip address associated with hostname and aliasses
+                IPHostEntry IpHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+                // read ip address from local address
+                IPAddress IpAddress = IpHostEntry.AddressList[1];
+                // class IPEndPoint combines port number and ip address
+                IPEndPoint localEndPoint = new IPEndPoint(IpAddress, 11111);
+
+                // create socket
+                Socket clientSocket = new Socket(IpAddress.AddressFamily
+                                                    , SocketType.Stream
+                                                    , ProtocolType.Tcp);
+
+                try
+                {
+                    // connect to server
+                    clientSocket.Connect(localEndPoint);
+
+                    Console.WriteLine("client connected to: {0}"
+                                        , clientSocket.RemoteEndPoint.ToString());
+
+                    // message to be sent via socket
+                    byte[] messageToSend = Encoding.ASCII.GetBytes("First connection<EOF>");
+                    // send message
+                    int returnValueOfSend = clientSocket.Send(messageToSend);
+
+                    byte[] encodedReceivedMessage = new byte[1024];
+
+                    // read from server
+                    int returnValueOfReicv = clientSocket.Receive(encodedReceivedMessage);
+                    string decodedReceivedMessage = Encoding.ASCII.GetString(encodedReceivedMessage);
+
+                    Console.WriteLine("server sent: {0}"
+                                        , decodedReceivedMessage);
+
+                    // shutdown and close connection
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+
+                } // Manage of Socket's Exceptions 
+                catch (ArgumentNullException ane)
+                {
+
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
--- 004_HomeMatic_EXE/Program.cs
+++ 004_HomeMatic_EXE/Program.cs
@@ -1,15 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace _004_HomeMatic_EXE
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace HomeMatic_EXE
 {
     class Program
     {
         static void Main(string[] args)
         {
+           
         }
+    }
+}
+
+namespace SocketConnection
+{
+    class ServerInformation
+    {
+        private IPAddress ipAddress;
+        private int port;
+
+        public ServerInformation(
+                            byte[] ipAddress,
+                            int port)
+        {
+            this.ipAddress = new IPAddress(ipAddress);
+            this.port = port;
+        }
+
+        public IPAddress getIpAddress()
+        {
+            return ipAddress;
+        }
+
+        public int getPort()
+        {
+            return port;
+        }
+    }
+
+    class Client
+    {
+        private IPAddress ipAddress;
+        private int port;
+        private ServerInformation serverInfo;
+
+        private Socket client;
+
+        public Client(
+                    byte[] ipAddress,
+                    int port,
+                    ServerInformation serverInfo
+                    )
+        {
+            this.ipAddress = new IPAddress(ipAddress);
+            this.port = port;
+            this.serverInfo = serverInfo;
+        }
+
+        public void EstablishConnection()
+        {
+            CreateClientSocket();
+
+            client.Connect(serverInfo.getIpAddress(),
+                            serverInfo.getPort());
+        }
+        private void CreateClientSocket()
+        {
+            this.client = new Socket(
+                                ipAddress.AddressFamily,
+                                SocketType.Stream,
+                                ProtocolType.Tcp);
+        }
+
+        public bool sendRequest(string requestString)
+        {
+            byte[] encodedRequest = Encoding.ASCII.GetBytes(requestString);
+            int quantityOfSentBytes = client.Send(encodedRequest);
+
+            return hasSameQuantityOfElements(
+                                encodedRequest.Length,
+                                quantityOfSentBytes);
+        }
+        private bool hasSameQuantityOfElements(int sentBytes,
+                                int answeredBytes)
+        {
+            return sentBytes == answeredBytes;
+        }
+
+        /* ... */
     }
 }
